74c3c9b4c62bb204e2bd35199bc40a5b73146256
Mickaël Véril <mika.veril@wanadoo.fr>
2022-07-05 00:42:53 +0200

    Validation ajouter client

--- src/main/java/servlets/AjouterClient.java
+++ src/main/java/servlets/AjouterClient.java
@@ -1,6 +1,9 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +14,7 @@ import dao.Dao;
 import dao.DaoException;
 import dao.DaoFactory;
 import modele.Client;
+import validation.ClientValidator;
 
 
 /**
@@ -46,21 +50,64 @@ public class AjouterClient extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
+		Map<String,String> erreurs = new HashMap<String,String>();
 		String companyName=request.getParameter("companyName");
+		var erreur = ClientValidator.validateCompanyName(companyName);
+		if(erreur != null) {
+			erreurs.put("companyName", erreur);
+		}
 		String firstName=request.getParameter("firstName");
+		erreur = ClientValidator.validateFirstName(firstName);
+		if(erreur != null) {
+			erreurs.put("companyName", erreur);
+		}
 		String lastName=request.getParameter("lastName");
+		erreur = ClientValidator.validateLastName(lastName);
+		if(erreur != null) {
+			erreurs.put("lastName", erreur);
+		}
         String email=request.getParameter("email");
+		erreur = ClientValidator.validateEmail(email);
+		if(erreur != null) {
+			erreurs.put("email", erreur);
+		}
         String phone=request.getParameter("phone");
+		erreur = ClientValidator.validatePhone(phone);
+		if(erreur != null) {
+			erreurs.put("phone", erreur);
+		}
         String address=request.getParameter("address");
-        String zipcode=request.getParameter("zipCode");
+		erreur = ClientValidator.validateAdress(address);
+		if(erreur != null) {
+			erreurs.put("address", erreur);
+		}
+        String zipCode=request.getParameter("zipCode");
+		erreur = ClientValidator.validateZipCode(zipCode);
+		if(erreur != null) {
+			erreurs.put("zipCode", erreur);
+		}
         String city=request.getParameter("city");
+		erreur = ClientValidator.validateCity(city);
+		if(erreur != null) {
+			erreurs.put("city", erreur);
+		}
         String country=request.getParameter("country");
+		erreur = ClientValidator.validateCity(country);
+		if(erreur != null) {
+			erreurs.put("country", erreur);
+		}
+		Long state = (long)-1;
         String stateStr=request.getParameter("state");
-        var state = Long.parseLong(stateStr);
-      
+		erreur = ClientValidator.validateCity("state");
+		if(erreur != null) {
+			erreurs.put("state", erreur);
+		} else {
+			state = Long.parseLong(stateStr);
+		}
         
-        
+		
+		
+		
         Client client= new Client();
         
         client.setCompany(companyName);
@@ -69,21 +116,25 @@ public class AjouterClient extends HttpServlet {
         client.setEmail(email);
         client.setPhone(phone);
         client.setAddress(address);
-        client.setZipCode(zipcode);
+        client.setZipCode(zipCode);
         client.setCity(city);
         client.setCountry(country);
         client.setState(state);
         
-        try {
-			clientDao.creer(client);
-		} catch (DaoException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(erreurs.isEmpty()) {
+			try {
+				clientDao.creer(client);
+			} catch (DaoException e) {
+				e.printStackTrace();
+			}
+			
+			response.sendRedirect( request.getContextPath() + "/listeClients" );
+		} else {
+			request.setAttribute("client", client);
+			request.setAttribute("erreurs", erreurs);
+			request.setAttribute("resultat", "Echec de la sauvegarde de du client.");
+			
+			this.getServletContext().getRequestDispatcher("/WEB-INF/ajouterClient.jsp").forward(request, response);
 		}
-        
-        
-        response.sendRedirect(request.getContextPath()+ "/listeClients");
-        		
 	}
-
 }
